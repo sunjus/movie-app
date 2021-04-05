50875e9d7426514ed5cf641a90a4a89406575eac
sunjus <sunju.shin1108@gmail.com>
2021-04-05 23:58:27 +0300

    Update Config.js to hide private key

--- src/components/Views/Config.js
+++ src/components/Views/Config.js
@@ -1,10 +1,10 @@
 
-export const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=0065d0c91554803d55345617612ad0b7&language=en-US';
+export const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
 
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w300';
 
 /*
-export const SEARCH_URL = 'https://api.themoviedb.org/3/search/company?api_key=0065d0c91554803d55345617612ad0b7&page=1';
+export const SEARCH_URL = `https://api.themoviedb.org/3/search/company?api_key=${process.env.REACT_APP_API_KEY}&page=1`;
 Top-rated : https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1
 Popular : https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 Latest : https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US
